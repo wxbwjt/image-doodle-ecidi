14dc2c83ba60319bf8113597f65872b46de9e928
韦锦涛 <wei_jt@ecidi.com>
2019-03-07 16:30:06 +0800

    '重新定义项目工程'

---
+++ app/src/main/java/com/example/wei_jt/image_doodle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wei_jt.image_doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
